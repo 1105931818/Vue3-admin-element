926e4268ff0b9921ddade207c9e9f5654639a8c0
maoPartTime <96091665+1105931818@users.noreply.github.com>
2023-06-07 01:53:53 +0800

    0607

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Child1 from "./components/Child1.vue";
+let money = ref(100);
+let page = ref("hello");
+/* const handler = (sum: number) => {
+  money.value = sum;
+}; */
+</script>
+
+<template>
+  <div class="main">
+    <h1>父组件：{{ money }}---{{ page }}</h1>
+    <input type="text" v-model="money" />
+    <!-- <Child1 :mode-value="money" @update:mode-value="handler" /> -->
+    <Child1 v-model:mode-value="money" v-model:page="page" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main {
+  width: 100%;
+  height: 400px;
+  background-color: aqua;
+  text-align: center;
+}
+</style>
---
+++ src/components/HintButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a :title="(titl.title as string | undefined)">
+    <el-button :="attrs">{{ title }}</el-button>
+  </a>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from "vue";
+
+const attrs = useAttrs();
+
+const titl = defineProps(["title"]);
+</script>
+
+<style scoped lang="scss"></style>
